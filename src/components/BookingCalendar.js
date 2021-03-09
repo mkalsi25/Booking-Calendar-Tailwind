2a449117431505610ffbd2f5c29f2477fcdaf81f
Manmeet <manmeetkalsi17@gmail.com>
2021-03-09 14:40:35 +0530

    update in design

--- src/components/BookingCalendar.js
+++ src/components/BookingCalendar.js
@@ -11,14 +11,16 @@ export default function BookingCalendar({ result, load }) {
                 key={key}
                 className={
                   avails.bookable
-                    ? "h-28 sm:h-44 bg-gradient-to-tr from-yellow-600 to-red-900 text-white flex items-end justify-center p-4 border border-white transform duration-200 ease-in-out scale-100 hover:scale-105"
-                    : "h-28 sm:h-44 bg-gray-400 flex items-end justify-center p-4 border border-gray-300 text-white"
+                    ? "h-28 sm:h-44 bg-gradient-to-tr from-yellow-600 to-red-900 text-white flex items-stretch justify-center border border-white transform duration-200 ease-in-out scale-100 hover:scale-105"
+                    : "h-28 sm:h-44 bg-gray-400 flex items-stretch justify-center border border-gray-300 text-white"
                 }
               >
-                <div>
-                  <h3 className="text-xl font-medium text-center">
+                <div className="self-start relative h-10 w-10 flex items-center justify-center -left-2 top-2 p-4 border-r border-b">
+                  <span className="text-xl font-medium text-center">
                     {new Date(avails.date).getDate()}
-                  </h3>
+                  </span>
+                </div>
+                <div className="self-end">
                   {avails.bookable === true && (
                     <p className="text-mobile md:text-xs font-medium text-white uppercase tracking-widest text-center">
                       Booking Available
